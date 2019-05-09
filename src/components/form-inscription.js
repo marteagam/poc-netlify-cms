41aed76f40196f010121960e6776fdabb59a15b6
Mario Arteaga <mario.arteaga@jalasoft.com>
2019-05-09 18:00:58 -0400

    Add extra inputs to query of grahql

--- src/components/form-inscription.js
+++ src/components/form-inscription.js
@@ -36,7 +36,6 @@ class FormInscription extends React.Component {
       formDescription,
       sections
     } = this.props.data.form.frontmatter
-    console.log('sections: ', sections);
 
     return (
       <>
@@ -136,196 +135,6 @@ class FormInscription extends React.Component {
               )
             })
           }
-          {/* <h4
-            style={{
-              color: '#B62940'
-            }}
-          >
-            Datos personales
-          </h4>
-          <div 
-            style={{
-                paddingLeft: 50
-              }}
-            >
-            
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Nombre completo</label>
-              <input type="text" name="name" onChange={this.handleUpdate} />
-              <p
-                style={{
-                  color: '#B62940',
-                  fontSize: 12,
-                  margin: 0
-                }}
-              >
-                La información de este campo será utilizada para tu certificado.
-              </p>
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Correo electrónico</label>
-              <input type="text" name="email" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Teléfono </label>
-              <input type="text" name="phone" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Ciudad</label>
-              <input type="text" name="city" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                marginBottom: 15
-              }}
-              onChange={this.handleUpdate}
-            >
-              <h5 
-                style={{
-                  marginBottom: 10,
-                  marginTop: 10,
-                }}
-              >
-                ¿Cuál es tu ocupación?
-              </h5>
-              <label>
-                Profesional
-                <input type="radio" value="professional" name="occupation" />
-              </label>
-              {` `}
-              <label>
-                Estudiante
-                <input type="radio" value="student" name="occupation" />
-              </label>
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Empresa en la que trabajas</label>
-              <input type="text" name="company" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Cargo</label>
-              <input type="text" name="position" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Tiempo de experiencia en años</label>
-              <select name="experience" onChange={this.handleUpdate} value={this.state.experience}>
-                <option value="< 1 año">{'< 1 año'}</option>
-                <option value="1 a 5 años">1 a 5 años</option>
-                <option value="5 a 10 años">5 a 10 años</option>
-                <option value="> 10 años">> 10 años</option>
-              </select>
-            </div>
-
-          </div>
-          
-          <h4
-            style={{
-              color: '#B62940'
-            }}
-          >
-            Datos de inscripción
-          </h4>
-
-          <div 
-            style={{
-              paddingLeft: 50
-            }}
-          >
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Paquete</label>
-              <select name="package" onChange={this.handleUpdate} value={this.state.package}>
-                <option value="TZ Xplorer">TZ Xplorer ( Bs. 249 )</option>
-              </select>
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Nombre para la factura</label>
-              <input type="text" name="nameBill" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>NIT ó CI para factura</label>
-              <input type="text" name="ci" onChange={this.handleUpdate} />
-            </div>
-
-            <div 
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                marginBottom: 15
-              }}
-            >
-              <label>Código de boleta de depósito bancario</label>
-              <input type="text" name="voucher" onChange={this.handleUpdate} />
-            </div>
-          </div> */}
           <input type="submit" value="Registrar" />
         </form>
       </>
@@ -347,6 +156,8 @@ export default props => (
               formFields {
                 label
                 placeholder
+                name
+                infoLabel
               }
             }
             buttonLabel
