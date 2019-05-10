5142a573f048880a0b9279722107e32edba88b02
Mario Arteaga <mario.arteaga@jalasoft.com>
2019-05-10 18:14:03 -0400

    Add connection with strapi for save form

--- src/services/inscription.js
+++ src/services/inscription.js
@@ -1,3 +1,22 @@
+import axios from 'axios';
+
+// export const isBrowser = () => typeof window !== "undefined"
+
+// export const getInscriptions = () =>
+//   isBrowser() && window.localStorage.getItem("inscriptions")
+//     ? JSON.parse(window.localStorage.getItem("inscriptions"))
+//     : []
+
+// const saveInscription = form => {
+//     var inscriptions = getInscriptions();
+//     inscriptions.push(form);
+//     window.localStorage.setItem("inscriptions", JSON.stringify(inscriptions))
+// }
+
+// export const handleInscription = (form) => {
+//     return saveInscription(form)
+// }
+
 export const isBrowser = () => typeof window !== "undefined"
 
 export const getInscriptions = () =>
@@ -9,8 +28,15 @@ const saveInscription = form => {
     var inscriptions = getInscriptions();
     inscriptions.push(form);
     window.localStorage.setItem("inscriptions", JSON.stringify(inscriptions))
+    // send to strapi
+    axios.post(`http://localhost:1337/inscriptions`, form)
+      .then(res => {
+        console.log('axios post: ', res);
+        console.log('axios data: ', res.data);
+      })
 }
 
 export const handleInscription = (form) => {
     return saveInscription(form)
 }
+
